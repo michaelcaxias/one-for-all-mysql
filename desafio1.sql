3273a421d85cd126d1de8708bb509637a4f0efac
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-04 18:52:23 -0300

    Desafio 1: Cria album e canção

--- desafio1.sql
+++ desafio1.sql
@@ -5,7 +5,7 @@ CREATE DATABASE SpotifyClone;
 -- https://stackoverflow.com/questions/4834390/how-to-use-mysql-decimal
 
 CREATE TABLE planos(
-    PLANO_ID INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    ID_PLANO INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
     TIPO_PLANO VARCHAR(15) NOT NULL DEFAULT 'Sem plano',
     VALOR_PLANO DECIMAL(7, 2) NOT NULL DEFAULT 0,
 ) engine = InnoDB;
@@ -18,23 +18,23 @@ VALUES
   ('Pessoal', '6.99');
 
 CREATE TABLE usuarios(
-    USUARIO_ID INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    ID_USUARIO INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
     NOME_USUARIO VARCHAR(50) NOT NULL DEFAULT 'Sem nome',
     IDADE INTEGER NOT NULL DEFAULT 1,
-    FOREIGN KEY `PLANO_ID` (PLANO_ID) REFERENCES `planos`(`PLANO_ID`) ON DELETE CASCADE
+    FOREIGN KEY `ID_PLANO_FK` (ID_PLANO) REFERENCES planos(`ID_PLANO`) ON DELETE CASCADE
 ) engine = InnoDB;
 
 INSERT INTO usuarios (NOME_USUARIO, IDADE)
 VALUES
-  ('Tathi', 23),
-  ('Cintia', 23),
-  ('Bill', 23),
-  ('Roger', 23),
-  ('Norman', 23);
+  ('Tathi'),
+  ('Cintia'),
+  ('Bill'),
+  ('Roger'),
+  ('Norman');
 
 
 CREATE TABLE artistas(
-    ARTISTA_ID INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    ID_ARTISTA INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
     NOME_ARTISTA VARCHAR(50) NOT NULL DEFAULT 'Artista Desconhecido',
 ) engine = InnoDB;
 
@@ -46,3 +46,43 @@ VALUES
   ('Peter Strong'),
   ('Tyler Isle'),
   ('Fog');
+
+CREATE TABLE albuns(
+    ID_USUARIO INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    NOME_ALBUM VARCHAR(50) NOT NULL DEFAULT 'Sem nome',
+    FOREIGN KEY `ID_ARTISTA` (ID_ARTISTA) REFERENCES artistas(ID_ARTISTA) ON DELETE CASCADE
+) engine = InnoDB;
+
+INSERT INTO albuns (NOME_ALBUM)
+VALUES
+  ('Envious'),
+  ('Exuberant'),
+  ('Hallowed Steam'),
+  ('Incandescent'),
+  ('Temporary Culture'),
+  ('Library of liberty'),
+  ('Chained Down'),
+  ('Cabinet of fools'),
+  ('No guarantees'),
+  ('Apparatus');
+
+CREATE TABLE cancoes(
+    ID_CANCAO INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    NOME_CANCAO VARCHAR(50) NOT NULL DEFAULT 'Sem nome',
+    DURACAO_CANCAO_SEGUNDOS INTEGER NOT NULL DEFAULT 0,
+    FOREIGN KEY `ID_ALBUM` (ID_ARTISTA) REFERENCES artistas(ID_ARTISTA) ON DELETE CASCADE
+    FOREIGN KEY `ID_ARTISTA` (ID_ARTISTA) REFERENCES artistas(ID_ARTISTA) ON DELETE CASCADE
+) engine = InnoDB;
+
+INSERT INTO cancoes (NOME_ALBUM)
+VALUES
+  ('Envious'),
+  ('Exuberant'),
+  ('Hallowed Steam'),
+  ('Incandescent'),
+  ('Temporary Culture'),
+  ('Library of liberty'),
+  ('Chained Down'),
+  ('Cabinet of fools'),
+  ('No guarantees'),
+  ('Apparatus');
