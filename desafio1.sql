73dcd00a95a5f39b63f103efa051e348adaa6422
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-04 18:28:50 -0300

    Desafio 1: Cria tabela de artistas

--- desafio1.sql
+++ desafio1.sql
@@ -32,19 +32,17 @@ VALUES
   ('Roger', 23),
   ('Norman', 23);
 
-CREATE TABLE tabela2(
-    coluna1 tipo restricoes,
-    coluna2 tipo restricoes,
-    colunaN tipo restricoes,
+
+CREATE TABLE artistas(
+    ARTISTA_ID INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    NOME_ARTISTA VARCHAR(50) NOT NULL DEFAULT 'Artista Desconhecido',
 ) engine = InnoDB;
 
-INSERT INTO tabela1 (coluna1, coluna2)
+INSERT INTO artistas (NOME_ARTISTA)
 VALUES
-  ('exemplo de dados 1', 'exemplo de dados A'),
-  ('exemplo de dados 2', 'exemplo de dados B'),
-  ('exemplo de dados 3', 'exemplo de dados C');
-
-INSERT INTO tabela2 (coluna1, coluna2)
-VALUES
-  ('exemplo de dados 1', 'exemplo de dados X'),
-  ('exemplo de dados 2', 'exemplo de dados Y');
+  ('Walter Phoenix'),
+  ('Freedie Shannon'),
+  ('Lance Day'),
+  ('Peter Strong'),
+  ('Tyler Isle'),
+  ('Fog');
