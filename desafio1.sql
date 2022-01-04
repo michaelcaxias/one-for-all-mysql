a23f8f633c5d2b685baf3afc2a3770b511847812
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-04 18:24:27 -0300

    Desafio 1: Cria tabela de usuários e plano

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,50 @@
+DROP DATABASE IF EXISTS SpotifyClone;
+
 CREATE DATABASE SpotifyClone;
+
+-- https://stackoverflow.com/questions/4834390/how-to-use-mysql-decimal
+
+CREATE TABLE planos(
+    PLANO_ID INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    TIPO_PLANO VARCHAR(15) NOT NULL DEFAULT 'Sem plano',
+    VALOR_PLANO DECIMAL(7, 2) NOT NULL DEFAULT 0,
+) engine = InnoDB;
+
+INSERT INTO planos (TIPO_PLANO, VALOR_PLANO)
+VALUES
+  ('Gratuito', '0'),
+  ('Familiar', '7.99'),
+  ('Universitario', '5.99'),
+  ('Pessoal', '6.99');
+
+CREATE TABLE usuarios(
+    USUARIO_ID INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    NOME_USUARIO VARCHAR(50) NOT NULL DEFAULT 'Sem nome',
+    IDADE INTEGER NOT NULL DEFAULT 1,
+    FOREIGN KEY `PLANO_ID` (PLANO_ID) REFERENCES `planos`(`PLANO_ID`) ON DELETE CASCADE
+) engine = InnoDB;
+
+INSERT INTO usuarios (NOME_USUARIO, IDADE)
+VALUES
+  ('Tathi', 23),
+  ('Cintia', 23),
+  ('Bill', 23),
+  ('Roger', 23),
+  ('Norman', 23);
+
+CREATE TABLE tabela2(
+    coluna1 tipo restricoes,
+    coluna2 tipo restricoes,
+    colunaN tipo restricoes,
+) engine = InnoDB;
+
+INSERT INTO tabela1 (coluna1, coluna2)
+VALUES
+  ('exemplo de dados 1', 'exemplo de dados A'),
+  ('exemplo de dados 2', 'exemplo de dados B'),
+  ('exemplo de dados 3', 'exemplo de dados C');
+
+INSERT INTO tabela2 (coluna1, coluna2)
+VALUES
+  ('exemplo de dados 1', 'exemplo de dados X'),
+  ('exemplo de dados 2', 'exemplo de dados Y');
