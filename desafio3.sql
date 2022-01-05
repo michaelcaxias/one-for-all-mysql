76d5cbbd8534aa22215529320999e3d5347f118d
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-05 18:01:14 -0300

    Desafio 3: Completo - Ajusta arrendondamento

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
 SELECT u.NOME_USUARIO AS usuario,
   COUNT(m.ID_USUARIO) AS qtde_musicas_ouvidas,
-  SUM(c.DURACAO_CANCAO_SEGUNDOS / 60) AS total_minutos
+  ROUND(SUM(c.DURACAO_CANCAO_SEGUNDOS / 60), 2) AS total_minutos
   FROM SpotifyClone.usuarios AS u
   INNER JOIN SpotifyClone.musicas_reproduzidas AS m
   ON (u.ID_USUARIO = m.ID_USUARIO)
