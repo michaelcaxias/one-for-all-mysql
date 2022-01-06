af105dcd4523291a6ebfb6222e1e60562bbb4f2d
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-06 10:16:09 -0300

    Desafio 5: Inicia lógica

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+SELECT c.NOME_CANCAO AS cancao,
+  COUNT(m.ID_CANCAO) AS reproducoes
+  FROM SpotifyClone.cancoes AS c
+  INNER JOIN SpotifyClone.musicas_reproduzidas AS m
+  ON m.ID_CANCAO = c.ID_CANCAO
+  GROUP BY c.NOME_CANCAO
+  ORDER BY reproducoes DESC;
