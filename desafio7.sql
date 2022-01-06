d27a31742e3bf5c94bcef921d26cb1e83cf0b989
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-06 10:52:12 -0300

    Desafio 7: Quase concluído

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+SELECT a.NOME_ARTISTA AS artista,
+  al.NOME_ALBUM AS album,
+  COUNT(s.ID_ARTISTA) AS seguidores
+  FROM SpotifyClone.artistas AS a
+  INNER JOIN SpotifyClone.albuns AS al
+  ON a.ID_ARTISTA = al.ID_ARTISTA
+  INNER JOIN SpotifyClone.seguindo_artistas AS s
+  ON a.ID_ARTISTA = s.ID_ARTISTA
+  GROUP BY artista, album
+  LIMIT 9;
