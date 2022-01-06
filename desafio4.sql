c5d8821bd68d4bcc0f73b677df97154257bf50ad
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-06 10:15:57 -0300

    Desafio 4: Ajustado

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 -- https://www.w3schools.com/sql/func_mysql_if.asp
 SELECT u.NOME_USUARIO AS usuario,
-  IF(MAX(YEAR(m.DATA_REPRODUCAO)) = 2021, 'Usuário ativo', 'Usuário inativo')
+  IF(MAX(YEAR(m.DATA_REPRODUCAO)) = 2021,'Usuário ativo', 'Usuário inativo')
   AS condicao_usuario
 FROM SpotifyClone.usuarios AS u
 INNER JOIN SpotifyClone.musicas_reproduzidas AS m
