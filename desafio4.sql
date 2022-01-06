7cb6dabe75b0873262527721d912bae3ed1619fe
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-05 21:12:30 -0300

    Desafio 4: Ajusta condição

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,5 @@
 SELECT u.NOME_USUARIO AS usuario,
-  IF(MAX(YEAR(m.DATA_REPRODUCAO)) = 2021, 'Usuário Ativo', 'Usuário Inativo') AS condicao_usuario
+  IF(MAX(YEAR(m.DATA_REPRODUCAO)) = 2021, 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
 FROM SpotifyClone.usuarios AS u
 INNER JOIN SpotifyClone.musicas_reproduzidas AS m
 ON u.ID_USUARIO = m.ID_USUARIO
